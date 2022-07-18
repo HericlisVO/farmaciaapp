ebfe29921c65f4cf8f45f8c8f97dfdcca465de04
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-18 20:46:00 -0300

    Criando um metodo getmapping

---
+++ src/main/java/com/generation/efarmacia/controller/ProdutoController.java
@@ -0,0 +1,28 @@
+package com.generation.efarmacia.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.efarmacia.model.Produto;
+import com.generation.efarmacia.repository.ProdutoRepository;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/produto")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+}
