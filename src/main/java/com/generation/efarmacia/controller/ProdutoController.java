0e7583d65eaec7d52b4d3df11e5694581a50b996
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-18 20:46:56 -0300

    criando um metodo de busca por id

--- src/main/java/com/generation/efarmacia/controller/ProdutoController.java
+++ src/main/java/com/generation/efarmacia/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,9 @@ public class ProdutoController {
 	public ResponseEntity<List<Produto>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
-	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
 }
