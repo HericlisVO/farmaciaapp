f2418c3b4af3839f82d1107e442c8936b2437220
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-18 20:34:09 -0300

    criando as classes model

---
+++ src/main/java/com/generation/efarmacia/FarmaciaappApplication.java
@@ -0,0 +1,13 @@
+package com.generation.efarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaappApplication.class, args);
+	}
+
+}
