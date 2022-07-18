2099235da68b3d4f8f48b835ea8d42d6feab411e
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-18 20:48:26 -0300

    criando um metodo especifico na classe produto repository e controller.

--- src/main/java/com/generation/efarmacia/repository/ProdutoRepository.java
+++ src/main/java/com/generation/efarmacia/repository/ProdutoRepository.java
@@ -1,9 +1,12 @@
 package com.generation.efarmacia.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.efarmacia.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
 }
